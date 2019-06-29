16f5392ff52263557e5288f3fa16055960d74dc1
PIC Benoit <benoit.pic@comdatagroup.fr>
2019-06-29 14:14:38 +0200

    First commit of vue-floatl

---
+++ src/VueFloatl.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="floatl" ref="floatl">
+        <label class="floatl__label">{{ label }}</label>
+        <input :name="name" class="floatl__input" :class="inputClass" :placeholder="label" :type="type" :value="value" @input="$emit('input', $event.target.value)" />
+    </div>
+</template>
+
+
+<script>
+    import Floatl from 'floatl';
+    export default {
+        props: {
+            value: {
+                default: ""
+            },
+            name: {
+                default: false
+            },
+            inputClass: {
+                default: ""
+            },
+            type: {
+                type: String,
+                default: "text"
+            },
+            label: {
+                type: String,
+                default: "My label"
+            }
+        },
+        mounted() {
+            let fl = new Floatl(this.$el);
+            this.$once('hook:beforeDestroy', () => fl.destroy())
+        }
+    }
+</script>
---
+++ src/plugin.js
@@ -0,0 +1,7 @@
+import VueFloatl from './VueFloatl.vue';
+
+module.exports = {
+  install: function (Vue, options) {
+    Vue.component('v-floatl', VueFloatl);
+  }
+};
